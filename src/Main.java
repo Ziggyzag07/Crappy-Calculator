2ce68c79827e2cc013ca23ecaad4ff35c6098172
Kyle Ashley <kyle11247@gmail.com>
2025-09-17 13:33:25 +0100

    updated commands

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         boolean Exit;
+        int x;
+        int y;
         Exit = false;
         System.out.println("Welcome to Crappy Calculator");
         while(Exit == false) {
             Scanner input = new Scanner(System.in);
             System.out.println("Please enter a number");
-            int x = input.nextInt();
+            if(input.hasNextInt()) {
+                x = input.nextInt();
+            }
+            else {
+                System.out.println("not a number");
+                continue;
+            }
             System.out.println("Please enter a second number");
-            int y = input.nextInt();
+            if(input.hasNextInt()) {
+                 y = input.nextInt();
+            }
+            else {
+                System.out.println("not a number");
+                continue;
+            }
             System.out.println("Please enter calculation");
             String command = input.next();
             switch (command){
@@ -22,16 +36,22 @@ public class Main {
                 case "sub":
                     new commands().sub(x, y);
                     continue;
-
-            }
-
-
-            else if(command.equals("exit")){
-                Exit = true;
-                continue;
+                case "mul":
+                    new commands().mul(x, y);
+                    continue;
+                case "div":
+                    new commands().div(x, y);
+                    continue;
+                case "remainder":
+                    new commands().remainder(x, y);
+                    continue;
+                case "exit":
+                    Exit = true;
+                    continue;
+                    default:
+                        System.out.println("Invalid command");
+                        continue;
             }
         }
-
-
     }
 }
